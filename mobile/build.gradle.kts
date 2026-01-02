e0cea32495bcd9f5367cd7151071c380ad4c4e60
ryugen04 <yamat.2667@gmail.com>
2026-01-02 22:58:35 +0900

    chore: bump version to 1.0.1 (versionCode 2)

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.dripsync.mobile"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.dripsync.wear"
         minSdk = libs.versions.minSdkWear.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
