1c8cd2c0dca62afe7ba45bda23d010204b2ab763
ryugen04 <yamat.2667@gmail.com>
2026-01-03 10:31:40 +0900

    feat: add Google Play internal testing auto-deploy

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.dripsync.mobile"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
